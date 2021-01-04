67638ad35efb84fc90667a1db840cf44a7bf3e9c
abhishek <abhishekps6282@gmail.com>
2021-01-04 12:49:18 +0530

    calender updated

---
+++ js/calender.js
@@ -0,0 +1,3 @@
+var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("month").innerHTML = months[d.getMonth()] + d.getFullYear();
